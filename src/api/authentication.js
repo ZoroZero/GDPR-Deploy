08bf5be7bd5c23240dbfc7751b22911cbec98da3
tutranminh9499 <minhtu.tran.09041999@gmail.com>
2020-09-04 09:14:46 +0700

    new url login

--- src/api/authentication.js
+++ src/api/authentication.js
@@ -4,11 +4,8 @@ export const loginApi = (username, password) => {
   return new Promise((resolve, reject) => {
     return axios
       .post(
-<<<<<<< HEAD
-        "/auth/login",
-=======
-        String(process.env.BASE_HOST) + "/auth/login",
->>>>>>> 96d40f9... add self env
+        String(process.env.REACT_APP_BASE_URL) + "/auth/login",
+
         {
           username: username,
           password: password,
