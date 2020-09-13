8f250027812e2578e0627b63e5823d2eab88e797
Duy An Ngo Nguyen <40843946+ZoroZero@users.noreply.github.com>
2020-09-13 18:05:43 +0700

    Fix list to only hold 1 file

--- src/components/ManageServer/ImportServer.js
+++ src/components/ManageServer/ImportServer.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, DatePicker, Button, Modal, Upload, message} from "antd";
 import { InboxOutlined } from '@ant-design/icons';
 import { checkToken } from "utils/localstorage";
 const { Dragger } = Upload;
 
 function ImportServer(props){
+    const [fileList, setFileList] = useState([]);
+    const [importFile, setImportFile] = useState('');
+    const handleChange = info => {
+      let fileList = [...info.fileList];
+  
+      // 1. Limit the number of uploaded files
+      // Only to show two recent uploaded files, and old ones will be replaced by the new
+      fileList = fileList.slice(-1);
+  
+      // 2. Read from response and show file link
+      fileList = fileList.map(file => {
+        if (file.response) {
+          // Component will show file.url as link
+          file.url = file.response.url;
+          setImportFile(file.response.filename)
+        }
+        return file;
+      });
+      // console.log("File list", fileList);
+      setFileList( fileList );
+      
+    };
+
     const uploadProps = {
       name:'file',
-      onChange: (info) => {
-          const { status } = info.file;
-          if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-          }
-          if (status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully.`);
-          } else if (status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-          }
-        },
+      // onChange: (info) => {
+      //     const { status } = info.file;
+      //     if (status !== 'uploading') {
+      //       console.log(info.file, info.fileList);
+      //     }
+      //     if (status === 'done') {
+      //       message.success(`${info.file.name} file uploaded successfully.`);
+      //     } else if (status === 'error') {
+      //       message.error(`${info.file.name} file upload failed.`);
+      //     }
+      // },
+      onChange: handleChange,
       action:'http://localhost:5000/api/servers/import',
       multiple: false,
       headers: {
         authorization: 'Bearer ' + checkToken(),
       },
+
     }
 
     const handleUpload = () => {
@@ -49,7 +74,7 @@ function ImportServer(props){
               </Button>
             ]}
             >
-            <Dragger {...uploadProps}>
+            <Dragger {...uploadProps} fileList={fileList}>
                 <p className="ant-upload-drag-icon">
                 <InboxOutlined />
                 </p>
