5944bad917fb869cd3a2b40fa86282fded4cd6d8
Hoang Dinh Khang <khang.hoangdinh@netpower.lan>
2020-09-21 09:37:37 +0700

    fix export request to not require end date start date

--- src/features/ManageRequest/pages/Main/index.js
+++ src/features/ManageRequest/pages/Main/index.js
@@ -17,14 +17,14 @@ import { Can } from "permission/can";
 const columns = [
   {
     title: "Number",
-    // width: "3%",
+    width: "5%",
     dataIndex: "Number",
     key: "number",
-    // fixed: "left",
+    fixed: "left",
   },
   {
     title: "Status",
-    // width: "5%",
+    width: "5%",
     dataIndex: "Status",
     key: "status",
     // fixed: "left",
@@ -45,7 +45,7 @@ const columns = [
   },
   {
     title: "Created Date",
-    // width: "20%",
+    width: "12%",
     dataIndex: "CreatedDate",
     key: "created-date",
     fixed: "left",
@@ -55,21 +55,21 @@ const columns = [
     title: "Updated Date",
     dataIndex: "UpdatedDate",
     key: "updated-date",
-    // width: "20%",
+    width: "12%",
     sorter: true,
   },
   {
     title: "Server",
     dataIndex: "Server",
     key: "server",
-    // width: "15%",
+    width: "10%",
     sorter: true,
   },
   {
     title: "Title",
     dataIndex: "Title",
     key: "title",
-    // width: "30%",
+    width: "30%",
     render: (data) => (
       <div>
         <div>
@@ -87,20 +87,21 @@ const columns = [
     title: "Request From",
     dataIndex: "StartDate",
     key: "request-from",
-    // width: 120,
+    width: "10%",
     sorter: true,
   },
   {
     title: "Request To",
     dataIndex: "EndDate",
     key: "request-to",
-    // width: 120,
+    width: "10%",
     sorter: true,
   },
   {
     title: "Action",
     key: "operation",
     dataIndex: "link",
+    width: "6%",
     // fixed: "right",
     render: (data) => (
       <Link to={data}>
@@ -210,7 +211,10 @@ const MainPage = (props) => {
         </Col>
       </Row>
       <Row gutter={[16, 16]} justify="center">
-        <Col span={10} offset={6}>
+        <Col>
+          <p>Total: {data.length > 0 ? data[0].Total : 0} items</p>
+        </Col>
+        <Col span={10}>
           <Pagination
             total={totalPage * pageSize}
             current={currentPage}
@@ -219,9 +223,6 @@ const MainPage = (props) => {
             showSizeChanger
             onShowSizeChange={onPageSizeChange}
           />
-        </Col>
-        <Col>
-          <p>Total: {data.length > 0 ? data[0].Total : 0} items</p>
         </Col>
       </Row>
     </>
